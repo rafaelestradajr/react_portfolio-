69216a87aec7603a9a109f3f1bbce00f741fbf2a
Rafael Estrada <rafaelestrada@comcast.net>
2020-05-20 17:24:56 -0700

    added gh pages files

---
+++ build/precache-manifest.e9ad26945fdab4a55909f11f573b1490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37d56ea780b0ca00e158f8bdb4a4e97",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "85916ce400daabd4712e",
+    "url": "/react_portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a5e6f63f50aa3b444add",
+    "url": "/react_portfolio/static/css/main.4d79f877.chunk.css"
+  },
+  {
+    "revision": "85916ce400daabd4712e",
+    "url": "/react_portfolio/static/js/2.5056b4fc.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react_portfolio/static/js/2.5056b4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e6f63f50aa3b444add",
+    "url": "/react_portfolio/static/js/main.102645bf.chunk.js"
+  },
+  {
+    "revision": "4901414a795d9e09b443",
+    "url": "/react_portfolio/static/js/runtime-main.08fd7df0.js"
+  }
+]);
